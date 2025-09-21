fc8b0c0e6f14f8c15e5a8008f39728cbd71ff0c5
Santiago Almiron <sanalmiron@hotmail.com>
2025-09-21 20:29:49 -0300

    FirstCommit

---
+++ src/grupo114_producto/codigobarras/Grupo114_ProductoCodigoBarras.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package grupo114_producto.codigobarras;
+
+/**
+ *
+ * @author santiago
+ */
+public class Grupo114_ProductoCodigoBarras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

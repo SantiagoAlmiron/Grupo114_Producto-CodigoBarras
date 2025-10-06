805ecee3584f5b600ae9dae88b80c362a7719eaa
Santiago Almiron <sanalmiron@hotmail.com>
2025-10-06 11:56:42 -0300

    Se crean las entidades base con sus atributos

--- src/config/Configuraciones.java
+++ src/config/Configuraciones.java
@@ -2,16 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package grupo114_producto.codigobarras;
+package config;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
 
 /**
  *
  * @author santiago
  */
-public class Test {
-    
-    
-    public String hello() {
-        return "Hola Mundo\n";
+public class Configuraciones {
+    public static ZoneId SetTimeZone() {
+        return ZoneId.of("America/Argentina/Buenos_Aires");
     }
 }
---
+++ src/services/Tiempo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+/**
+ *
+ * @author santiago
+ */
+public class Tiempo { 
+    private static ZoneId SetTimeZone() {
+        return ZoneId.of("America/Argentina/Buenos_Aires");
+    }
+    
+    public static ZonedDateTime ahora() {
+        return ZonedDateTime.now(SetTimeZone());
+    }
+}
